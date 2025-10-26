3a9b21fabff006b2e886dfe52bfe25d9a40a2ba6
deven <devenshona@gmail.com>
2025-10-26 11:54:48 -0500

    Initialize BlockRegistry and ItemRegistry in the mod init process

--- common/src/main/java/com/github/spacemex/allthecrops/Allthecrops.java
+++ common/src/main/java/com/github/spacemex/allthecrops/Allthecrops.java
@@ -1,9 +1,13 @@
 package com.github.spacemex.allthecrops;
 
+import com.github.spacemex.allthecrops.registry.BlockRegistry;
+import com.github.spacemex.allthecrops.registry.ItemRegistry;
+
 public final class Allthecrops {
     public static final String MOD_ID = "allthecrops";
 
     public static void init() {
-        // Write common init code here.
+        ItemRegistry.init();
+        BlockRegistry.init();
     }
 }
